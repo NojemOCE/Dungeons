724d9b74e9ce174d10b1bc2b0a0f53a9708a4d9d
Jeffrey Meng <jeffrey.meng@student.unsw.edu.au>
2021-10-18 19:47:24 +1100

    Added stubs for all subclasses of CollectibleEntities

---
+++ src/main/java/dungeonmania/collectable/InvincibilityPotion.java
@@ -0,0 +1,11 @@
+package dungeonmania.collectable;
+
+public class InvincibilityPotion extends CollectableEntities {
+    private int duration;
+
+    public void consume() {};
+
+    public void tick() {};
+
+    public void invincibility() {};
+}
