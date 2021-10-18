0ff38648df0b51db9f4a6d11d2823dc05a4ac1d0
levincent51 <levincent51@gmail.com>
2021-10-18 21:18:10 +1100

    finish battle

---
+++ src/main/java/dungeonmania/character/Battle.java
@@ -0,0 +1,20 @@
+package dungeonmania.character;
+
+public class Battle {
+    public Player player;
+    public Character character;
+
+
+    public Battle(Player player, Character character) {
+        this.player = player;
+        this.character = character;
+    }
+
+    public void battleTick() {
+
+    }
+
+    public void endBattle() {
+        
+    }
+}
