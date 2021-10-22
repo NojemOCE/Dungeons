8048fdcc05766321cb0410e8f2173d49762bb2a1
Claire <clairehua148@gmail.com>
2021-10-23 01:36:54 +1100

    finished boulder; fixed typo

--- src/main/java/dungeonmania/staticEntity/Boulder.java
+++ src/main/java/dungeonmania/staticEntity/Boulder.java
@@ -1,8 +1,8 @@
 package dungeonmania.staticEntity;
 
+import java.util.List;
+
 import dungeonmania.World;
-import dungeonmania.goal.ObserverTreasureGoal;
-import dungeonmania.goal.SubjectBoulderSwitchGoal;
 import dungeonmania.movingEntity.MovingEntity;
 import dungeonmania.movingEntity.Player;
 import dungeonmania.response.models.EntityResponse;
@@ -25,9 +25,28 @@ public class Boulder extends StaticEntity {
 
         if (character instanceof Player) {
             Position toMoveBoulderTo = move(character);
-            // check that nothing is in this spot (in layer one)
-            // need a list of ALL entities?
-            // map to check what entities are on that spot
+            List<StaticEntity> entitiesAtNewPos = world.getStaticEntitiesAtPosition(toMoveBoulderTo);
+            // if anything is on the same layer, can't move
+            if (entitiesAtNewPos.stream().anyMatch(x -> x.getPosition().getLayer() == this.getPosition().getLayer())) {
+                return character.getPosition();
+            }
+
+            // otherwise move
+            List<StaticEntity> entitiesAtThisPos = world.getStaticEntitiesAtPosition(this.getPosition());
+            // untrigger any switch in the previous spot
+            entitiesAtThisPos.stream()
+                            .filter(x -> x instanceof FloorSwitch)
+                            .map(FloorSwitch.class::cast)
+                            .forEach(x -> x.untrigger());
+
+            // trigger any switch in the new spot
+            entitiesAtNewPos.stream()
+                            .filter(x -> x instanceof FloorSwitch)
+                            .map(FloorSwitch.class::cast)
+                            .forEach(x -> x.trigger());
+            // move boulder then return appropriate position for character to move to
+            this.setPosition(toMoveBoulderTo);
+            return this.getPosition();
         }
         
         
@@ -66,6 +85,7 @@ public class Boulder extends StaticEntity {
         }
     }
 
+    // Not sure this is required
     public void move(Direction d) {
 
     }
