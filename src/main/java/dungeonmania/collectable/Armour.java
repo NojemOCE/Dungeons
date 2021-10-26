7cd0f5d53fa2ce4c11c27ac9e9c82fcfd50d0496
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-10-26 23:47:58 +1100

    updated constructor for armour

--- src/main/java/dungeonmania/collectable/Armour.java
+++ src/main/java/dungeonmania/collectable/Armour.java
@@ -7,13 +7,10 @@ import dungeonmania.util.Position;
 
 public class Armour extends CollectableEntity {
 
-    private String itemId;
-    private String type = "armour";
     private final double DEFENCE_MULTIPLIER = 0.5;
 
     public Armour(Position position, String itemId) {
-        super(position);
-        this.itemId = itemId;
+        super(position, itemId, "armour");
     }
 
     public double defenceModifier() {
@@ -22,11 +19,11 @@ public class Armour extends CollectableEntity {
 
     @Override
     public EntityResponse getEntityResponse() {
-        return new EntityResponse(itemId, type, getPosition(), false);
+        return new EntityResponse(getItemId(), getType(), getPosition(), !isCollected());
     }
 
     @Override
     public ItemResponse getItemResponse() {
-        return new ItemResponse(itemId, type);
+        return new ItemResponse(this.getItemId(), this.getType());
     }
 }
