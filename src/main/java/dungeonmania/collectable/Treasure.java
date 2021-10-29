f7c17a88ae33401aa04814b89931dc1b9437e289
Jeffrey Meng <jeffrey.meng@student.unsw.edu.au>
2021-10-29 17:22:08 +1100

    Refactored wood

--- src/main/java/dungeonmania/collectable/Treasure.java
+++ src/main/java/dungeonmania/collectable/Treasure.java
@@ -2,7 +2,7 @@ package dungeonmania.collectable;
 import dungeonmania.Consumable;
 import dungeonmania.inventory.Inventory;
 
-public class Treasure extends CollectableEntity implements Consumable {
+public class Treasure extends CollectableEntity implements {
 
     public Treasure(int x, int y, String itemId) {
         super(x, y, itemId, "treasure");
