64c15026f5d222d3231fb100c82733abd4b702b6
Jeffrey Meng <jeffrey.meng@student.unsw.edu.au>
2021-10-29 17:21:27 +1100

    Refactored Treasure

--- src/main/java/dungeonmania/collectable/Treasure.java
+++ src/main/java/dungeonmania/collectable/Treasure.java
@@ -4,12 +4,7 @@ import dungeonmania.inventory.Inventory;
 
 public class Treasure extends CollectableEntity implements Consumable {
 
-    public Treasure(int x, int y, String itemId, Inventory inventory) {
-        super(x, y, itemId, "treasure", inventory);
+    public Treasure(int x, int y, String itemId) {
+        super(x, y, itemId, "treasure");
     }
-
-    public void consume() {
-        getInventory().removeItem(getId());
-    }
-
 }
