1c1b50ec3d0c9e7009c5e5babbf03c7cd124d3de
Jeffrey Meng <jeffrey.meng@student.unsw.edu.au>
2021-10-29 17:20:01 +1100

    Refactored shield

--- src/main/java/dungeonmania/collectable/Shield.java
+++ src/main/java/dungeonmania/collectable/Shield.java
@@ -1,53 +1,20 @@
 package dungeonmania.collectable;
 
 import dungeonmania.Consumable;
-import dungeonmania.buildable.Buildable;
 import dungeonmania.response.models.ItemResponse;
 
-public class Shield extends CollectableEntity implements Consumable {
+public class Shield extends CollectableEntity {
 
-    private String type = "shield";
-    private String itemId;
-    private int durability;
-    private boolean built = false;
     private final int DURABILITY = 10;
     private final int DEFENCE = 10;
 
-    public Shield(String itemId) {
-        this.itemId = itemId;
-    };
-
-    public void build() {
-        this.durability = DURABILITY;
-        this.built = true;
-    };
-
-    public int defenceModifier() {
-        if (built) {
-            return DEFENCE;
-        }
-        return 1;
-    };
-
-    public void consume() {
-        this.durability--;
-        if (this.durability == 0) {
-            this.built = false;
-        }
-    };
-
-    public boolean isBuilt() {
-        return this.built;
+    public Shield(int x, int y, String itemId) {
+        super(x, y, itemId, "shield");
+        setDurability(DURABILITY);
     }
 
-    @Override
-    public ItemResponse getItemResponse() {
-        return new ItemResponse(itemId, type);
+    public int defenceModifier() { 
+        return DEFENCE;
     }
 
-    @Override
-    public String getItemId() {
-        // TODO Auto-generated method stub
-        return itemId;
-    }
 }
