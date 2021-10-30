be4b4dde59aff93464c739ade357f596e4ce0eaf
Mia Dobell <z5254542@ad.unsw.edu.au>
2021-10-30 23:14:21 +1100

    boulder goals test added

--- src/test/java/dungeonmania/GoalsTest.java
+++ src/test/java/dungeonmania/GoalsTest.java
@@ -67,12 +67,8 @@ public class GoalsTest {
         d = c.tick(null, Direction.DOWN);
         assertEquals(":exit", d.getGoals());
 
-        
+        // Move to exit
         d = c.tick(null, Direction.DOWN);
-        assertEquals(":exit", d.getGoals());
-
-        
-        d = c.tick(null, Direction.RIGHT);
         assertEquals("", d.getGoals());
     }
 
@@ -84,22 +80,66 @@ public class GoalsTest {
         DungeonResponse d = c.newGame("simple-and-goal", "Standard");
         assertEquals("(:enemies AND :exit)", d.getGoals());
 
-        d = c.tick(null, Direction.RIGHT);
-        assertEquals("(:enemies AND :exit)", d.getGoals());
-
         // Player moves to the top of the exit
         d = c.tick(null, Direction.RIGHT);
         assertEquals("(:enemies AND :exit)", d.getGoals());
 
         
         // Mercenary and player meet on the same square (battle)
-        d = c.tick(null, Direction.LEFT);
+        d = c.tick(null, Direction.UP);
         assertEquals("(:enemies AND :exit)", d.getGoals());
 
         //Battle ended, enemies goal achieved
         d = c.tick(null, Direction.DOWN);
         assertEquals(":exit", d.getGoals());
         
+        d = c.tick(null, Direction.DOWN);
+        assertEquals("", d.getGoals());
+    }
+
+    @Test
+    public void simpleBoulderExitTest1(){
+        DungeonManiaController c = new DungeonManiaController();
+        // Check if the player goes on exit before the other goals are achieved, that the goal is not completed
+        
+        DungeonResponse d = c.newGame("simple-boulders", "Standard");
+        assertEquals("(:exit OR :boulders)", d.getGoals());
+
+        // Player pushes one boulder onto a switch (note, in this map, one boulder is created on top of a switch)
+        d = c.tick(null, Direction.DOWN);
+        assertEquals("(:exit OR :boulders)", d.getGoals());
+
+        
+        // Player moves up
+        d = c.tick(null, Direction.UP);
+        assertEquals("(:exit OR :boulders)", d.getGoals());
+
+        // Player pushes the last boulder onto the switch
+        d = c.tick(null, Direction.RIGHT);
+        assertEquals("", d.getGoals());
+    }
+
+    @Test
+    public void simpleBoulderExitTest2(){
+        DungeonManiaController c = new DungeonManiaController();
+        // Check if the player goes on exit before the other goals are achieved, that the goal is not completed
+        
+        DungeonResponse d = c.newGame("simple-boulders", "Standard");
+        assertEquals("(:exit OR :boulders)", d.getGoals());
+
+        // Player pushes one boulder onto a switch (note, in this map, one boulder is created on top of a switch)
+        d = c.tick(null, Direction.DOWN);
+        assertEquals("(:exit OR :boulders)", d.getGoals());
+
+        
+        // Player moves right
+        d = c.tick(null, Direction.RIGHT);
+        assertEquals("(:exit OR :boulders)", d.getGoals());
+
+        // Player moves down
+        d = c.tick(null, Direction.DOWN);
+        assertEquals("(:exit OR :boulders)", d.getGoals());
+
         d = c.tick(null, Direction.RIGHT);
         assertEquals("", d.getGoals());
     }
