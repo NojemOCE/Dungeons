b0a2f180c670b69b8d5aff6d30fd5fb98fc19478
Jeffrey Meng <jeffrey.meng@student.unsw.edu.au>
2021-10-30 13:15:15 +1100

    pulling master

--- src/main/java/dungeonmania/collectable/OneRing.java
+++ src/main/java/dungeonmania/collectable/OneRing.java
@@ -8,10 +8,5 @@ public class OneRing extends CollectableEntity {
     public OneRing(int x, int y, String itemId) {
         super(x, y, itemId, "one_ring");
     }
-<<<<<<< HEAD
-=======
-
-    //TODO do we need to do anything to use the one ring or nah
->>>>>>> master
 
 }
