6892c0dc44c8ae997afb535b61df7671c5a052c2
Jeffrey Meng <jeffrey.meng@student.unsw.edu.au>
2021-10-30 17:43:43 +1100

    All collectables don't have errors now

--- src/main/java/dungeonmania/collectable/Armour.java
+++ src/main/java/dungeonmania/collectable/Armour.java
@@ -1,19 +1,13 @@
 package dungeonmania.collectable;
 
-import org.json.JSONObject;
-
-import dungeonmania.inventory.Inventory;
-
 public class Armour extends CollectableEntity {
 
-    private String itemId;
     private final int DURABILITY = 7;
 
     private final double DEFENCE_MULTIPLIER = 0.5;
 
     public Armour(int x, int y, String itemId) {
         super(x, y, itemId, "armour");
-        this.itemId = itemId;
         setDurability(DURABILITY);;
     }
 
