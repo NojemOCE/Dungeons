945cd0b95f27473f3823cec9d3dd02f70bae6726
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-11-01 02:06:42 +1100

    started controller testing for inventory

---
+++ src/test/java/dungeonmania/InventoryControllerTesting.java
@@ -0,0 +1,37 @@
+package dungeonmania;
+
+import org.junit.jupiter.api.Test;
+
+public class InventoryControllerTesting {
+
+    /**
+     * When collecting an item, an item should appear in the inventory
+     */
+    /**
+     * When collecting an item, an item should appear in the inventory
+     */
+    @Test
+    public void testCollectItem() {
+        DungeonManiaController controller = new DungeonManiaController();
+    }
+
+    /**
+     * When consuming a consumable, it should no longer appear in the inventory
+     */
+
+    /**
+     * When crafting a bow, it should appear in the inventory
+     * The items used to craft it should not appear in the inventory
+     */
+
+    /**
+     * When crafting a shield, it should appear in the inventory
+     * The items used to craft it should not appear in the inventory
+     */
+
+    /**
+     * Ensure that Buildable list updates correctly
+     * as items are collected
+     */
+
+}
