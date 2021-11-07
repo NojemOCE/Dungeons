08e4b2e099b1ec15aef411ccce15b4db1be45005
Claire <clairehua148@gmail.com>
2021-11-08 00:57:52 +1100

    added basic swamp testing

--- src/test/java/dungeonmania/staticEntityTest/SwampTileTest.java
+++ src/test/java/dungeonmania/staticEntityTest/SwampTileTest.java
@@ -1,19 +1,166 @@
 package dungeonmania.staticEntityTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import dungeonmania.World;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
 import dungeonmania.staticEntity.SwampTile;
+import dungeonmania.util.Direction;
+import dungeonmania.util.FileLoader;
 import dungeonmania.util.Position;
 
 public class SwampTileTest {
     @Test
     public void constructionTest(){
-
         Position swampPos = new Position(1,1);
-        SwampTile swamp = new SwampTile(swampPos.getX(), swampPos.getY(), "swamp1");
+        int movement_factor = 2;
+        SwampTile swamp = new SwampTile(swampPos.getX(), swampPos.getY(), "swamp1",  movement_factor);
         assertNotNull(swamp);
-        assert(swampPos.equals(portal.getPosition()));
-    
+        assert(swampPos.equals(swamp.getPosition()));
     }
 
+    /**
+     * Check that boulders can be pushed onto a swamp tile
+     * MAP:
+     * P S 
+     *   B
+     */
+    @Test
+    public void boulderTest() {
+        // Create a new world
+        World world = new World("simple_swamp", "Standard");
+        try {
+            String file = FileLoader.loadResourceFile("/dungeons/" + "simple_swamp" + ".json");
+            JSONObject game = new JSONObject(file);
+            world.buildWorld(game);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // move player into position to push boulder
+        world.tick(null, Direction.DOWN);
+        world.tick(null, Direction.DOWN);
+        DungeonResponse d = world.tick(null, Direction.RIGHT);
+        List<EntityResponse> entities = d.getEntities();
+
+        // get the current positions
+        Position playerPos = null;
+        Position boulderPos = null;
+        for (EntityResponse er : entities) {
+            if (er.getType().equals("player")) {
+                playerPos = er.getPosition();
+            } else if (er.getType().equals("boulder")) {
+                boulderPos = er.getPosition();
+            }
+        }
+
+        // check that we got the positions
+        assertNotNull(playerPos);
+        assertNotNull(boulderPos);
+
+        // move up and confirm player and boulder can both move up
+        d = world.tick(null, Direction.UP);
+        entities = d.getEntities();
+
+        // get positions after tick
+        Position playerPos2 = null;
+        Position boulderPos2 = null;
+        for (EntityResponse er : entities) {
+            if (er.getType().equals("player")) {
+                playerPos2 = er.getPosition();
+            } else if (er.getType().equals("boulder")) {
+                boulderPos2 = er.getPosition();
+            }
+        }
+
+        // check that we got the positions
+        assertNotNull(playerPos2);
+        assertNotNull(boulderPos2);
+
+
+        assert(playerPos2.equals(playerPos.translateBy(Direction.UP)));
+        assert(boulderPos2.equals(boulderPos.translateBy(Direction.UP)));
+
+    }
+
+
+    /**
+     * Check that player can walk onto swamp tile and it takes extra time
+     * MAP:
+     * P S 
+     *   B
+     */
+    @Test
+    public void playerMovementTest() {
+        // Create a new world
+        World world = new World("simple_swamp", "Standard");
+        try {
+            String file = FileLoader.loadResourceFile("/dungeons/" + "simple_swamp" + ".json");
+            JSONObject game = new JSONObject(file);
+            world.buildWorld(game);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // move player into swamp tile
+        DungeonResponse d = world.tick(null, Direction.RIGHT);
+        List<EntityResponse> entities = d.getEntities();
+
+        // get the current positions
+        Position playerPos = null;
+        for (EntityResponse er : entities) {
+            if (er.getType().equals("player")) {
+                playerPos = er.getPosition();
+            }
+        }
+
+        // check that we got the position
+        assertNotNull(playerPos);
+
+        // movement factor is 2 so we should stay on the tile for 2 moves
+        d = world.tick(null, Direction.RIGHT);
+        entities = d.getEntities();
+
+        // get positions after tick
+        Position playerPos2 = null;
+        for (EntityResponse er : entities) {
+            if (er.getType().equals("player")) {
+                playerPos2 = er.getPosition();
+            }
+        }
+
+        // check that we got the positions
+        assertNotNull(playerPos2);
+
+        assert(playerPos2.equals(playerPos));
+
+        // now if we move one more time then we should be out of the tile
+        // movement factor is 2 so we should stay on the tile for 2 moves
+        d = world.tick(null, Direction.RIGHT);
+        entities = d.getEntities();
+
+        // get positions after tick
+        playerPos2 = null;
+        for (EntityResponse er : entities) {
+            if (er.getType().equals("player")) {
+                playerPos2 = er.getPosition();
+            }
+        }
+
+        // check that we got the positions
+        assertNotNull(playerPos2);
+
+        assert(playerPos2.equals(playerPos.translateBy(Direction.RIGHT)));
+    }
+
+
+    // TODO: force enemies to walk on the swamp to test movement and make sure they are also slowed down
 }
