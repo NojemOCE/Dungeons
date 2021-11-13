20adccd2bb00cf64eb5c3aadcab0ef78a66befc3
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-11-13 17:35:09 +1100

    added cooldown tests

--- src/test/java/dungeonmania/sceptreTest.java
+++ src/test/java/dungeonmania/sceptreTest.java
@@ -62,6 +62,50 @@ public class sceptreTest {
 
     @Test
     public void testCooldown() {
+        DungeonManiaController controller = new DungeonManiaController();
+        controller.newGame("sceptreMindControl", "standard");
+        controller.tick(null, Direction.RIGHT);
+        controller.tick(null, Direction.RIGHT);
+        controller.tick(null, Direction.RIGHT);
+        controller.build("sceptre");
 
+        // Mind control the mercenary
+        assertDoesNotThrow(() -> controller.interact("mercenary5"));
+
+        // Move down for 3 ticks
+        for (int i = 0; i < 3; i++) {
+            DungeonResponse d = controller.tick(null, Direction.DOWN);
+            for (EntityResponse e : d.getEntities()) {
+                if (e.getId().equals("mercenary5")) {
+                    assertFalse(e.isInteractable());
+                }
+                assertThrows(InvalidActionException.class, () -> controller.interact("mercenary6"));
+            }
+        }
+
+        // Collect coin
+        controller.tick(null, Direction.DOWN);
+        assertDoesNotThrow(() -> controller.interact("mercenary6"));
+
+        // Move down for 6 ticks
+        for (int i = 0; i < 6; i++) {
+            DungeonResponse d = controller.tick(null, Direction.DOWN);
+            for (EntityResponse e : d.getEntities()) {
+                if (e.getId().equals("mercenary5") || e.getId().equals("mercenary6")) {
+                    assertFalse(e.isInteractable());
+                }
+            }
+        }
+
+        // After 10 ticks, the mercenary is no longer mind controlled
+        // The mercenary that is bribed should still be an ally
+        DungeonResponse notControlled = controller.tick(null, Direction.DOWN);
+        for (EntityResponse e : notControlled.getEntities()) {
+            if (e.getId().equals("mercenary5")) {
+                assertTrue(e.isInteractable());
+            } else if (e.getId().equals("mercenary6")) {
+                assertFalse(e.isInteractable());
+            }
+        }
     }
 }
