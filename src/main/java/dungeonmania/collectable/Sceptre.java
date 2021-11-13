f594ea57d143f83fd438b4d35adb25ce53efff4e
Nicholas Lam <nicholas.lam@student.unsw.edu.au>
2021-11-13 16:13:56 +1100

    bug fixing sceptre

--- src/main/java/dungeonmania/collectable/Sceptre.java
+++ src/main/java/dungeonmania/collectable/Sceptre.java
@@ -15,14 +15,16 @@ import static java.util.Objects.isNull;
 public class Sceptre extends CollectableEntity {
     final int DURATION = 10;
     private Map<MercenaryComponent, Integer> controlled;
+    private int cooldown = 0;
 
     public Sceptre(int x, int y, String itemId) {
         super(x, y, itemId, "sceptre");
         controlled = new HashMap<>();
     }
 
-    public Sceptre(String itemId, int durability) {
+    public Sceptre(String itemId, int cooldown, int durability) {
         this(0, 0, itemId, durability);
+        this.cooldown = cooldown;
     }
 
     public Sceptre(int x, int y, String itemId, int durability) {
@@ -30,7 +32,20 @@ public class Sceptre extends CollectableEntity {
         setDurability(durability);
     }
 
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
+    }
+
+    public void tickCooldown() {
+        if (getCooldown() > 0) setCooldown(getCooldown() - 1);
+    }
+
     public void useMindControl(MercenaryComponent m) {
+        setCooldown(15);
         useMindControl(m, DURATION);
     }
 
@@ -41,6 +56,7 @@ public class Sceptre extends CollectableEntity {
     }
 
     public void tickMindControlled() {
+        tickCooldown();
         controlled.replaceAll((m, v) -> v - 1);
         notifyMindControlled();
     }
@@ -70,9 +86,20 @@ public class Sceptre extends CollectableEntity {
             JSONObject entity = new JSONObject();
             entity.put("id", m.getId());
             entity.put("duration", getDuration(m));
+            controlledJSON.put(entity);
         }
-
         return controlledJSON;
+    }
+
+    public boolean onCD() {
+        return cooldown == 0;
+    }
+
+    @Override
+    public JSONObject saveGameJson() {
+        JSONObject sceptreJSON  = super.saveGameJson();
+        sceptreJSON.put("cooldown", "0");
+        return sceptreJSON;
     }
 
     @Override
--- src/test/java/dungeonmania/sceptreTest.java
+++ src/test/java/dungeonmania/sceptreTest.java
@@ -3,6 +3,8 @@ package dungeonmania;
 import dungeonmania.collectable.*;
 import dungeonmania.exceptions.InvalidActionException;
 import dungeonmania.inventory.Inventory;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
 import dungeonmania.util.Direction;
 import org.junit.jupiter.api.Test;
 
@@ -38,12 +40,23 @@ public class sceptreTest {
 
         // Mind control the mercenary
         assertDoesNotThrow(() -> controller.interact("mercenary5"));
-        assertDoesNotThrow(() -> controller.interact("mercenary6"));
-        // Craft sceptre
-        // Bribe mercenary
-        // Bribe assassin
-        // Make sure they are controlled
-        // Tick 10 times
-        // Make sure they are both not controlled
+
+        // For 10 ticks, the mercenary is uncontrolled
+        for (int i = 0; i < 10; i++) {
+            DungeonResponse d = controller.tick(null, Direction.DOWN);
+            for (EntityResponse e : d.getEntities()) {
+                if (e.getId().equals("mercenary5")) {
+                    assertFalse(e.isInteractable());
+                }
+            }
+        }
+
+        // After 10 ticks, the mercenary is no longer mind controlled
+        DungeonResponse notControlled = controller.tick(null, Direction.RIGHT);
+        for (EntityResponse e : notControlled.getEntities()) {
+            if (e.getId().equals("mercenary5")) {
+                assertTrue(e.isInteractable());
+            }
+        }
     }
 }
